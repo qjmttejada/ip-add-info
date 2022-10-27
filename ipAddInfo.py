887750b5b85bd278ae7fb4ad3ac238231ee037fa
qjmttejada <qjmttejada@tip.edu.ph>
2022-10-27 05:37:12 +0000

    with GUI

--- ipAddInfo.py
+++ ipAddInfo.py
@@ -1,34 +1,18 @@
-import requests
-import ipaddress
-    
-#API to get the info of the computer's IP Address
-def get_info(ip_address):
-    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
-    location_data = {
-        "ip": ip_address,
-        "city": response.get("city"),
-        "region": response.get("region"),
-        "country": response.get("country_name"),
-        "postal_code": response.get("postal"),
-        "isp": response.get("org")
-    }
-    return location_data
+import ipapi
+from flask import Flask, request, render_template
+ 
+#pip install ipapi
+ 
+ 
+app = Flask(__name__)
+ 
+ 
+@app.route('/', methods = ['GET', 'POST'])
+def Index():
+    search = request.form.get('search')
+    data = ipapi.location(ip=search, output='json')
+    return render_template('index.html', data=data)
+ 
 
-def validate_ip_address(address):
-    try:
-        ip = ipaddress.ip_address(address)
-        return True
-    except ValueError:
-        return False
-
-#Main functon
-while True:
-    response = input("Enter IP Address: ")
-    validate = validate_ip_address(response)
-    if response == "quit" or response == "q":
-        break
-    if validate == True:
-        info = get_info(response)
-        print(info)
-    else:
-        print("invalid input")
+if __name__ == "__main__":
+    app.run(debug=True)
