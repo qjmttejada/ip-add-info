411bbd9418bec57033a9e26042ef695d99c08f27
qjmttejada <qjmttejada@tip.edu.ph>
2022-10-27 03:54:46 +0000

    added isp

--- ipAddInfo.py
+++ ipAddInfo.py
@@ -1,11 +1,11 @@
 import requests
 
-
+#API to get the computer's IP Address
 def get_ip():
     response = requests.get('https://api64.ipify.org?format=json').json()
     return response["ip"]
 
-
+#API to get the info of the computer's IP Address
 def get_info():
     ip_address = get_ip()
     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
@@ -13,7 +13,9 @@ def get_info():
         "ip": ip_address,
         "city": response.get("city"),
         "region": response.get("region"),
-        "country": response.get("country_name")
+        "country": response.get("country_name"),
+        "postal_code": response.get("postal"),
+        "isp": response.get("org")
     }
     return location_data
 
