ab0d076e6a58d5c6ae0e8dffdff1067789d1d6b6
qjmttejada <qjmttejada@tip.edu.ph>
2022-11-03 04:30:51 +0000

    docker container

--- ipAddInfo.sh
+++ ipAddInfo.sh
@@ -4,18 +4,18 @@ mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
 
-cp sample_app.py tempdir/.
+cp ipAddInfo.py tempdir/.
 cp -r templates/* tempdir/templates/.
 
 echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "RUN pip install ipapi" >> tempdir/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY ipAddInfo.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/ipAddInfo.py" >> tempdir/Dockerfile
 
 cd tempdir
-docker build -t ipAddInfo .
-docker run -t -d -p 5050:5050 --name samplerunning ipAddInfo
-docker ps -a 
+docker build -t ipaddinfo .
+docker run -t -d -p 5050:5050 --name ipapirunning ipaddinfo
+docker ps -a
